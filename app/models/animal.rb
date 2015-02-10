adffbac69628eeae2590ba4810ee591b9124dc64
Vance Conway <vance_8d@hotmail.com>
2015-02-10 07:31:50 +0000

    added seed data

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,5 +2,11 @@ class Animal < ActiveRecord::Base
 belongs_to :classification
 has_many :CommonNames
 
+class 
+def recent?
+if @extinction_date >= 1900
+true
+else 
+false
 
 end
--- db/migrate/20150206214424_create_common_names.rb
+++ db/migrate/20150206214424_create_common_names.rb
@@ -1,7 +1,8 @@
 class CreateCommonNames < ActiveRecord::Migration
   def change
     create_table :common_names do |t|
-	  t.references 
+	  t.references :animal
+	  t.string :name
       t.timestamps null: false
     end
   end
